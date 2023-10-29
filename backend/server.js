fe1a41aa04f30330bd0a9194a5bf71744f6dbe6d
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-10-29 09:53:50 +0200

    add setup

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require("module-alias/register");
+const mongoose = require("mongoose");
+
+// Make sure we are running node 7.6+
+const [major, minor] = process.versions.node.split(".").map(parseFloat);
+if (major < 14 || (major === 14 && minor <= 0)) {
+  console.log(
+    "Please go to nodejs.org and download version 8 or greater. 👌\n "
+  );
+  process.exit();
+}
+
+// import environmental variables from our variables.env file
+require("dotenv").config({ path: ".variables.env" });
+
+// Connect to our Database and handle any bad connections
+// mongoose.connect(process.env.DATABASE);
+
+mongoose.connect(
+  "mongodb+srv://ahmedkhaled4d:565656@covacluster0.qbavl02.mongodb.net/Zazu?retryWrites=true&w=majority"
+);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on("error", (error) => {
+  console.error(`🚫 Error → : ${error.message}`);
+});
+
+const glob = require("glob");
+const path = require("path");
+
+glob.sync("./models/**/*.js").forEach(function (file) {
+  require(path.resolve(file));
+});
+
+// Start our app!
+const app = require("./app");
+app.set("port", process.env.PORT || 8888);
+const server = app.listen(app.get("port"), () => {
+  console.log(`Express running → On PORT : ${server.address().port}`);
+});
